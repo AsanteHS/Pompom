3e0f524f2fc7c37f3ffbd12368bc3c6c68818a56
Matías Incem <mincem@devartis.com>
2017-08-01 11:39:20 -0300

    Add QR code generation to huddle board

--- pompom/apps/huddle_board/urls.py
+++ pompom/apps/huddle_board/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import HuddleBoardView
+from .views import HuddleBoardView, PlaceholderView
 
 urlpatterns = [  # pylint: disable=invalid-name
     url(r'^huddle_board/$', HuddleBoardView.as_view(), name="huddle_board"),
+    url(r'^observe_card/$', PlaceholderView.as_view(), name="observe_card"),
 ]
--- pompom/apps/huddle_board/views.py
+++ pompom/apps/huddle_board/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HuddleBoardView(TemplateView):
     template_name = 'pompom/huddle_board.html'
+
+
+class PlaceholderView(TemplateView):
+    template_name = 'pompom/huddle_board.html'
