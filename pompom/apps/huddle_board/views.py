8f91b713303f9813fb391c5e100f0fe5ba63af77
Matías Incem <mincem@devartis.com>
2017-08-01 15:01:58 -0300

    Modify observation template to display a card from database

--- pompom/apps/huddle_board/views.py
+++ pompom/apps/huddle_board/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from pompom.apps.huddle_board.models import Card
+
 
 class HuddleBoardView(TemplateView):
     template_name = 'huddle_board/huddle_board.html'
@@ -7,3 +9,8 @@ class HuddleBoardView(TemplateView):
 
 class PerformObservationView(TemplateView):
     template_name = 'huddle_board/observation.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['card'] = Card.objects.first()
+        return context
