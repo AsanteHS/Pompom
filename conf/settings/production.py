073e83e3247ef943daa58123c3cdd74d15a7ec33
Fernando Aramendi <fernando@devartis.com>
2017-08-07 11:49:08 -0300

    updated prod settings

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -12,37 +12,25 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env('DATABASE_HOST'),
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-    }
-}
+# Parse database configuration from $DATABASE_URL
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
-MEDIA_ROOT = env('MEDIA_ROOT')
-STATIC_ROOT = env('STATIC_ROOT')
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+#MEDIA_ROOT = env('MEDIA_ROOT')
+#STATIC_ROOT = env('STATIC_ROOT')
+#SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pompom-prod.herokuapp.com']
 
 RAVEN_CONFIG = {
-    'dsn': env('RAVEN_DSN', default=""),
+    'dsn': os.environ['RAVEN_DSN'],
 }
 
-INSTALLED_APPS += 'raven.contrib.django.raven_compat',
+INSTALLED_APPS = INSTALLED_APPS + ('raven.contrib.django.raven_compat',)
 # static file serving for heroku
 MIDDLEWARE += ('whitenoise.middleware.WhiteNoiseMiddleware',)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST', default="")
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', default="")
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default="")
-EMAIL_USE_TLS = True
