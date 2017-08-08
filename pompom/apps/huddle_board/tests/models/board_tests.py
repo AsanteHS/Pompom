2d9a4938103651b52a5b9a9c57bd50130e678aa7
Matías Incem <mincem@devartis.com>
2017-08-08 15:02:55 -0300

    Delete obsolete shuffling tests

--- pompom/apps/huddle_board/tests/models/board_tests.py
+++ pompom/apps/huddle_board/tests/models/board_tests.py
@@ -19,17 +19,3 @@ class TestBoard:
     @pytest.fixture
     def a_board(self, a_deck):
         return Board.objects.create(title='a board', deck=a_deck)
-
-    def test_draw_card_returns_each_card_from_deck(self, a_board, three_cards):
-        three_drawn_cards = {a_board.draw_card() for _ in range(3)}
-        assert three_drawn_cards == set(three_cards)
-
-    def test_fourth_drawn_card_causes_reshuffle(self, a_board, three_cards):
-        for _ in range(3):
-            a_board.draw_card()
-        fourth_drawn_card = a_board.draw_card()
-        assert fourth_drawn_card in three_cards
-
-    def test_shuffle_board(self, a_board):
-        a_board.reshuffle()
-        assert a_board.draw_pile.count() == 3
