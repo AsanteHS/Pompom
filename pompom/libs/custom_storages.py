d0d79627e036fdeb14d74f696aeb06b75afe608a
Fernando Aramendi <fernando@devartis.com>
2017-08-10 16:34:21 -0300

    disabled pep8 abstract warning

--- pompom/libs/custom_storages.py
+++ pompom/libs/custom_storages.py
@@ -1,16 +1,11 @@
+# pylint: disable=W0223
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
 
 class StaticStorage(S3BotoStorage):
-    def path(self, name):
-        pass
-
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3BotoStorage):
-    def path(self, name):
-        pass
-
     location = settings.MEDIAFILES_LOCATION
