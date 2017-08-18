40c7c3106a22624c7314dbd08fbb397a97a798bb
Fernando Aramendi <fernando@devartis.com>
2017-08-18 16:17:36 -0300

    added production token key

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -18,6 +18,7 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
+MOBILE_TOKEN_KEY = env('MOBILE_TOKEN_KEY')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
