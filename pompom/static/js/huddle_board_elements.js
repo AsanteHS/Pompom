68d7dce6286a7805cc22fe1b9cd9d98df5a7f140
Matías Incem <mincem@devartis.com>
2017-08-29 11:12:49 -0300

    Add js code to preload images and redraw cards

---
+++ pompom/static/js/huddle_board_elements.js
@@ -0,0 +1,46 @@
+function CardsRetriever(viewURL){
+    var seconds = 15;
+    setInterval(retrieveCards, seconds * 1000, viewURL);
+}
+
+function retrieveCards(viewURL){
+    $.ajax({
+        url : viewURL,
+        type : "GET",
+        success : function(data) {
+            preloadImages(
+                getImagesFromHTML(data),
+                function(){refreshCardsOnScreen(data);}
+            );
+        }
+    });
+}
+
+function preloadImages(arrayOfImages, callback) {
+    var i;
+    var arrayLength = arrayOfImages.length;
+    var loaded = 0;
+
+    for (i = 0; i < arrayLength; i++){
+        (function (img, src) {
+            img.onload = function () {
+                if (++loaded === arrayLength && callback) {
+                    callback();
+                }
+            };
+            img.onerror = function () {};
+            img.onabort = function () {};
+            img.src = src;
+        } (new Image(), arrayOfImages[i]));
+    }
+}
+
+function getImagesFromHTML(data) {
+    var dummy = $('<div></div>');
+    dummy.html(data);
+    return $('img', dummy).map(function() { return this.src; });
+}
+
+function refreshCardsOnScreen(data) {
+    $('#cards-container').html(data);
+}
