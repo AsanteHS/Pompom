93845ee93e43d89c3849bc12e94bcc8a31170d8c
akang <ak1306@hotmail.com>
2018-03-02 12:00:03 -0300

    Added export action for answers.

--- pompom/apps/huddle_board/admin.py
+++ pompom/apps/huddle_board/admin.py
@@ -5,9 +5,81 @@ from ordered_model.admin import OrderedTabularInline
 from solo.admin import SingletonModelAdmin
 from taggit.models import Tag
 from taggit_helpers.admin import TaggitListFilter
+from import_export import resources, fields
+from import_export.admin import ExportActionModelAdmin
+from import_export.fields import Field
 
 from pompom.apps.huddle_board.forms import CardForm, DeckForm
 from .models import Card, CardSection, Observation, Answer, Board, Deck, CardNote, SafetyMessage, SiteConfiguration
+
+
+class ObservationResource(resources.ModelResource):
+
+    class Meta:
+        model = Observation
+        fields = ('created', 'board', 'card')
+
+
+class CardResource(resources.ModelResource):
+    decks = fields.Field(column_name='Contained in decks')
+    sections = fields.Field(column_name='Sections')
+    title = fields.Field(column_name='Title')
+
+    class Meta:
+        model = Card
+        fields = ('tags')
+
+    def dehydrate_title(self, card):
+        return card.title
+
+    def dehydrate_sections(self, card):
+        return ', '.join(section.title or '(no title)' for section in card.sections.all())
+
+    def dehydrate_decks(self, card):
+        return ', '.join(deck.title for deck in card.decks.all())
+
+
+class AnswerResource(resources.ModelResource):
+    observation = fields.Field(column_name='Observation')
+    card_section = fields.Field(column_name='Card section')
+    card_section_contents = fields.Field(column_name='Contents')
+    grade = fields.Field(column_name='Grade')
+    checks_done = fields.Field(column_name='Checks done')
+    board = fields.Field(column_name='Board')
+    card = fields.Field(column_name='Card')
+
+    class Meta:
+        model = Answer
+        fields = ('')
+
+    def dehydrate_card_section_contents(self, answer):
+        return answer.card_section.contents
+
+    def dehydrate_board(self, answer):
+        return answer.observation.board
+
+    def dehydrate_observation(self, answer):
+        return answer.observation
+
+    def dehydrate_card_section(self, answer):
+        return answer.card_section
+
+    def dehydrate_grade(self, answer):
+        grade = {True: 'Pass', False: 'Fallout', None: 'N/A'}
+        return grade[answer.grade]
+
+    def dehydrate_checks_done(self, answer):
+        return answer.checks_done
+
+    def dehydrate_card(self, answer):
+        return answer.observation.card.title
+
+
+class SafetyMessageResource(resources.ModelResource):
+
+    class Meta:
+        model = SafetyMessage
+        fields = ('created', 'modified', 'contents')
 
 
 class CardSectionInline(OrderedTabularInline):
@@ -120,3 +192,9 @@ class TagAdmin(admin.ModelAdmin):
     ordering = ["name", "slug"]
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
+
+
+@admin.register(Answer)
+class AnswerAdmin(ExportActionModelAdmin, admin.ModelAdmin):
+    list_display = ('__str__', 'card_section')
+    resource_class = AnswerResource
