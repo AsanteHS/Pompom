4b5085bdbfffae0332ac25c9bc2d167d62d69ed2
akang <ak1306@hotmail.com>
2018-03-28 11:57:27 -0300

    fixed proxytag permissions bug.

---
+++ pompom/apps/huddle_board/management/commands/fix_permissions.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals, absolute_import, division
+
+import sys
+
+from django.contrib.auth.management import _get_all_permissions
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
+from django.apps import apps
+from django.utils.encoding import smart_text
+
+
+def fix(self, *args, **options):
+    for model in apps.get_models():
+        opts = model._meta
+        ctype, created = ContentType.objects.get_or_create(
+            app_label=opts.app_label,
+            model=opts.object_name.lower())
+        for codename, name in _get_all_permissions(opts):
+            p, created = Permission.objects.get_or_create(
+                codename=codename,
+                content_type=ctype,
+                defaults={'name': name})
+            if created:
+                sys.stdout.write('Adding permission {}\n'.format(p))
